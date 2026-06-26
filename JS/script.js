0a61e0c9d488847ad6b8d1acfa1624fbe402e63b
Umesh-MishraRB <umishrax89@gmail.com>
2026-06-27 03:02:11 +0530

    JS first video

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+alert("Hello World");
+
+console.log("The code is running")
+console.log("The code is also running another time")
+console.log("The code is running like a wow")
+console.log("The code is running again like wow")
+
+
+
+var a= prompt("Enter the number to proceed please: ")
+console.log("The value of a"+" "+a)
+
+var isTrue= confirm("Are you serious to blast your computer: ")
+
+if( isTrue){
+    console.log("The computer is blasting")
+
+}
+else{
+    console.log("The computer is not blasting")
+}
